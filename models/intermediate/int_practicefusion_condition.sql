9a96435ab6c7224dc9b49401415a6f162853f02c
Ridge Walker <ridge.walker@vytalizehealth.com>
2025-06-10 23:55:57 -0400

    DE-1905: fix errors

--- models/intermediate/int_practicefusion_condition.sql
+++ models/intermediate/int_practicefusion_condition.sql
@@ -4,7 +4,7 @@ select
 	cast(diagnosis.patient_id as {{dbt.type_string()}}) as patient_id,
 	cast(diagnosis.encounter_id as {{dbt.type_string()}}) as encounter_id,
 	cast(null as {{dbt.type_string()}}) as claim_id,
-	CASE WHEN LENGTH(TRIM(substring(diagnosis.recorded_datetime, 1, 8))) = 8 AND TRIM(substring(diagnosis.recorded_datetime, 1, 8)) NOT LIKE '%-%' AND TRIM(substring(diagnosis.recorded_datetime, 1, 8)) NOT LIKE '%/%' THEN to_date(substring(problem_list.recorded_datetime, 1, 8), 'yyyyMMdd')
+	CASE WHEN LENGTH(TRIM(substring(diagnosis.recorded_datetime, 1, 8))) = 8 AND TRIM(substring(diagnosis.recorded_datetime, 1, 8)) NOT LIKE '%-%' AND TRIM(substring(diagnosis.recorded_datetime, 1, 8)) NOT LIKE '%/%' THEN to_date(substring(diagnosis.recorded_datetime, 1, 8), 'yyyyMMdd')
 		ELSE NULL
     END AS recorded_date,
 	CASE WHEN LENGTH(TRIM(diagnosis.start_date)) = 8 AND TRIM(diagnosis.start_date) NOT LIKE '%-%' AND TRIM(diagnosis.start_date) NOT LIKE '%/%' THEN to_date(TRIM(diagnosis.start_date), 'yyyyMMdd')
@@ -20,7 +20,7 @@ select
 	cast(null as {{dbt.type_string()}}) as status,
 	cast('diagnosis'  as {{dbt.type_string()}}) as condition_type,
 	cast(diagnosis.diagnosis_code_system as {{dbt.type_string()}}) as source_code_type,
-	coalesce(diagnosis.diagnosis_code) as source_code,
+	diagnosis.diagnosis_code as source_code,
 	cast(diagnosis.diagnosis_name as {{dbt.type_string()}}) as source_description,
 	cast(null as {{dbt.type_string()}}) as normalized_code_type,
 	cast(null as {{dbt.type_string()}}) as normalized_code,
@@ -41,7 +41,7 @@ select
 	cast(problem_list.patient_id as {{dbt.type_string()}}) as patient_id,
 	cast(problem_list.encounter_id as {{dbt.type_string()}}) as encounter_id,
 	cast(null as {{dbt.type_string()}}) as claim_id,
-	CASE WHEN LENGTH(TRIM(substring(problem_list.recorded_datetime, 1, 8))) = 8 AND TRIM(substring(problem_list.recorded_datetime, 1, 8)) NOT LIKE '%-%' AND TRIM(substring(problem_list.recorded_datetime, 1, 8)) NOT LIKE '%/%' THEN to_date(substring(problem_list.recorded_datetime, 1, 8)), 'yyyyMMdd')
+	CASE WHEN LENGTH(TRIM(substring(problem_list.recorded_datetime, 1, 8))) = 8 AND TRIM(substring(problem_list.recorded_datetime, 1, 8)) NOT LIKE '%-%' AND TRIM(substring(problem_list.recorded_datetime, 1, 8)) NOT LIKE '%/%' THEN to_date(substring(problem_list.recorded_datetime, 1, 8), 'yyyyMMdd')
  		ELSE NULL
     END AS recorded_date,
 	CASE WHEN LENGTH(TRIM(substring(problem_list.start_date, 1, 8))) = 8 AND TRIM(substring(problem_list.start_date, 1, 8)) NOT LIKE '%-%' AND TRIM(substring(problem_list.start_date, 1, 8)) NOT LIKE '%/%' THEN to_date(TRIM(substring(problem_list.start_date, 1, 8)), 'yyyyMMdd')
@@ -53,7 +53,7 @@ select
 	cast(null as {{dbt.type_string()}}) as status,
 	cast('problem_list'  as {{dbt.type_string()}}) as condition_type,
 	cast(problem_list.condition_code_system as {{dbt.type_string()}}) as source_code_type,
-	coalesce(problem_list.condition_code) as source_code,
+	problem_list.condition_code as source_code,
 	cast(problem_list.condition_name as {{dbt.type_string()}}) as source_description,
 	cast(null as {{dbt.type_string()}}) as normalized_code_type,
 	cast(null as {{dbt.type_string()}}) as normalized_code,
